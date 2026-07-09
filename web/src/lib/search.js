35e37f3db07726790ba2574edea5623125c3e4bd
craigrallen <56693437+craigrallen@users.noreply.github.com>
2026-07-09 02:34:56 +0200

    chore: add French search aliases
    Co-authored-by: Craig Allen <craig@interconnected.au>

--- web/src/lib/search.js
+++ web/src/lib/search.js
@@ -23,11 +23,11 @@ const QUERY_STOPWORDS = new Set([
 ]);
 
 const CATEGORY_ALIASES = {
-  emergency: ['emergency', 'emergency number', 'emergency services', 'police', 'policia', 'ambulance', 'fire', 'bomberos', '112', '911', '999', '000'],
-  child_protection: ['child protection', 'childline', 'youth'],
-  domestic_violence: ['domestic violence', 'domestic abuse', 'violencia domestica', 'dv'],
-  suicide_crisis: ['suicide crisis', 'suicide', 'suicidal'],
-  mental_health: ['mental health', 'salud mental'],
+  emergency: ['emergency', 'emergency number', 'emergency services', 'police', 'policia', 'ambulance', 'fire', 'bomberos', 'pompiers', 'urgence', 'numero urgence', '112', '911', '999', '000'],
+  child_protection: ['child protection', 'childline', 'youth', 'protection enfance', 'enfance danger'],
+  domestic_violence: ['domestic violence', 'domestic abuse', 'violencia domestica', 'violence domestique', 'violences conjugales', 'dv'],
+  suicide_crisis: ['suicide crisis', 'suicide', 'suicidal', 'suicidaire'],
+  mental_health: ['mental health', 'salud mental', 'sante mentale'],
   gambling: ['gambling', 'gambling help'],
   sexual_violence: ['sexual violence', 'sexual assault', 'rape crisis', 'rape support'],
   human_trafficking: ['human trafficking', 'trafficking'],
@@ -62,9 +62,10 @@ const CATEGORY_LABELS = {
 
 const COUNTRY_ALIASES = {
   france: ['francia'],
-  spain: ['espana'],
-  'united kingdom': ['uk'],
-  'united states': ['usa', 'united states', 'us'],
+  spain: ['espana', 'espagne'],
+  germany: ['allemagne'],
+  'united kingdom': ['uk', 'royaume uni'],
+  'united states': ['usa', 'united states', 'us', 'etats unis'],
   'united arab emirates': ['uae'],
 };
 
